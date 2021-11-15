b696275d32f582a532a0aa0aee83b775ef880ac5
ankase <ankase18@gmail.com>
2021-11-15 19:29:31 +0800

    BROADCASTRECEIVER

--- app/src/main/java/com/neverova/project/view/MainActivity.kt
+++ app/src/main/java/com/neverova/project/view/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.neverova.project.view
 
+import android.content.BroadcastReceiver
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.neverova.project.*
 import com.neverova.project.databinding.ActivityMainBinding
 import com.neverova.project.data.entity.Film
+import com.neverova.project.receivers.ConnectionChecker
 import com.neverova.project.view.fragments.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var receiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,18 @@ class MainActivity : AppCompatActivity() {
             .addToBackStack(null)
             .commit()
 
+        receiver = ConnectionChecker()
+        val filters = IntentFilter().apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_BATTERY_LOW)
+        }
+        registerReceiver(receiver, filters)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 
     fun launchDetailsFragment(film: Film) {
@@ -77,6 +94,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun checkFragmentExistence(tag: String): Fragment? = supportFragmentManager.findFragmentByTag(tag)
 
     private fun changeFragment(fragment: Fragment, tag: String) {
